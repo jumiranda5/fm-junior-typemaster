34113aed0c009a70e3f1e7c4a7a6f4293a1cde74
Juliana <jumiranda0501@gmail.com>
2023-12-28 15:59:34 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+'use-strict';
+
+import './styles/reset.css';
+import './styles/style.css';
+
+/*
+import Image from '../assets/images/image.jpg';
+const myImage = document.getElementById('id');
+myImage.src = Image;
+*/
